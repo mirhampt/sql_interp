032ae3ec69a2a679cfc966ee53a91de6c7c2ae77
Michael Hampton <mirhampt@gmail.com>
2009-10-27 19:57:16 -0400

    Rewrite to OO style

--- sql_interp.py
+++ sql_interp.py
@@ -1,84 +1,51 @@
 from esc_types import Esc, ListEsc, DictEsc
 
-TYPE_MAP = { 
-    list: ListEsc,
-    tuple: ListEsc,
-    dict: DictEsc,
-}
-
 class UnknownTypeError(Exception): pass
 
-def sql_interp(*args):
-    """
-    Interpolate python variables into SQL based upon context.
+class SQLInterp(object):
+    TYPE_MAP = { 
+        list: ListEsc,
+        tuple: ListEsc,
+        dict: DictEsc,
+    }
 
-    Indirect port of Perl's SQL::Interp.
+    def __init__(self):
+        pass
 
-    Example:
+    def interp(self, *args):
+        sql = ""
+        bind = ()
 
-    >>> item = { 'first_name': 'John', 'last_name': 'Doe' }
-    >>> sql_interp("INSERT INTO table", item)
-    ('INSERT INTO table (first_name, last_name) VALUES (?, ?)', ('John', 'Doe'))
-    >>> sql_interp("UPDATE table SET", item, "WHERE y <>", 2)
-    ('UPDATE table SET first_name = ?, last_name = ? WHERE y <> ?', ('John', 'Doe', 2))
-    >>> sql_interp("DELETE FROM table WHERE y =", 2)
-    ('DELETE FROM table WHERE y = ?', (2,))
-    
-    # The following two examples produce the same result.
-    >>> sql_interp("SELECT * FROM table WHERE x =", 3, "AND y IN", [1,2,3])
-    ('SELECT * FROM table WHERE x = ? AND y IN (?, ?, ?)', (3, 1, 2, 3))
-    >>> sql_interp("SELECT * FROM table WHERE", { 'x': 3, 'y': [1, 2, 3] })
-    ('SELECT * FROM table WHERE x = ? AND y IN (?, ?, ?)', (3, 1, 2, 3))
+        def _append_sql(sql, part):
+            "Handle whitespace when appending properly."
+            if len(sql) == 0:
+                return part
+            elif sql[-1] == ' ':
+                return sql + part
+            else:
+                return sql + ' ' + part
 
-    CAUTION: If the value to be interpolated is a string, you must pass it to the esc()
-    function first or it will be treated as raw SQL.  Forgetting to do this could pose
-    a security risk.
+        for arg in args:
+            if type(arg) is str:
+                # Strings are treated as raw SQL.
+                sql = _append_sql(sql, arg)
+            elif isinstance(arg, Esc):
+                # If this is an instance of Esc, ask the object
+                # how to represent the data given the context.
+                arg_sql, arg_bind = arg.to_string(sql)
+                sql = _append_sql(sql, arg_sql)
+                bind += arg_bind
+            else:
+                # Any argument given that is not a string or Esc
+                # is an error.
+                arg_sql, arg_bind = self.esc(arg).to_string(sql)
+                sql = _append_sql(sql, arg_sql)
+                bind += arg_bind
 
-    >>> first_name = 'John'
+        return (sql, bind)
 
-    # The following is wrong!  This could lead to a SQL injection attack!
-    >>> sql_interp("SELECT * FROM table WHERE first_name =", first_name)
-    ('SELECT * FROM table WHERE first_name = John', ())
-
-    # This is the correct way.
-    >>> sql_interp("SELECT * FROM table WHERE first_name =", esc(first_name))
-    ('SELECT * FROM table WHERE first_name = ?', ('John',))
-    """
-    sql = ""
-    bind = ()
-
-    def _append_sql(sql, part):
-        "Handle whitespace when appending properly."
-        if len(sql) == 0:
-            return part
-        elif sql[-1] == ' ':
-            return sql + part
+    def esc(self, val):
+        if type(val) in self.TYPE_MAP:
+            return self.TYPE_MAP[type(val)](val)
         else:
-            return sql + ' ' + part
-
-    for arg in args:
-        if type(arg) is str:
-            # Strings are treated as raw SQL.
-            sql = _append_sql(sql, arg)
-        elif isinstance(arg, Esc):
-            # If this is an instance of Esc, ask the object
-            # how to represent the data given the context.
-            arg_sql, arg_bind = arg.to_string(sql)
-            sql = _append_sql(sql, arg_sql)
-            bind += arg_bind
-        else:
-            # Any argument given that is not a string or Esc
-            # is an error.
-            arg_sql, arg_bind = esc(arg).to_string(sql)
-            sql = _append_sql(sql, arg_sql)
-            bind += arg_bind
-
-    return (sql, bind)
-
-def esc(val):
-    """
-    """
-    if type(val) in TYPE_MAP:
-        return TYPE_MAP[type(val)](val)
-    else:
-        return Esc(val)
+            return Esc(val)
--- tests/test_sql_interp.py
+++ tests/test_sql_interp.py
@@ -1,75 +1,92 @@
 from nose.tools import eq_, ok_, raises
-from sql_interp import esc, sql_interp, UnknownTypeError
+from sql_interp import UnknownTypeError, SQLInterp
 from esc_types import Esc, ListEsc, DictEsc
 
-### Test esc() function.
+### Test SQLInterp.esc() method.
 
 def test_esc_int():
-    obj = esc(1)
+    sqli = SQLInterp()
+    obj = sqli.esc(1)
     eq_(type(obj), Esc)
 
 def test_esc_str():
-    obj = esc('')
+    sqli = SQLInterp()
+    obj = sqli.esc('')
     eq_(type(obj), Esc)
 
 def test_esc_dict():
-    obj = esc({})
+    sqli = SQLInterp()
+    obj = sqli.esc({})
     eq_(type(obj), DictEsc)
 
 def test_esc_list():
-    obj = esc([])
+    sqli = SQLInterp()
+    obj = sqli.esc([])
     eq_(type(obj), ListEsc)
 
 def test_esc_tuple():
     # Tuples are treated identically to lists.
-    obj = esc((1,))
+    sqli = SQLInterp()
+    obj = sqli.esc((1,))
     eq_(type(obj), ListEsc)
 
-### Test sql_interp() function.
+### Test SQLInterp.interp() method.
 
-def test_sql_interp_no_whitespace():
+def test_sql_interp_no_whitespace2():
     # Whitespace should be added between arguments.
-    sql, bind = sql_interp("SELECT", "*", "FROM table")
+    sqli = SQLInterp()
+
+    sql, bind = sqli.interp("SELECT", "*", "FROM table")
     eq_(sql, "SELECT * FROM table")
     eq_(bind, ())
 
     cols = ['one', 'two', 'three']
-    sql, bind = sql_interp("SELECT", cols, "FROM table")
+    sql, bind = sqli.interp("SELECT", cols, "FROM table")
     eq_(sql, "SELECT (?, ?, ?) FROM table")
     eq_(bind, ('one', 'two', 'three'))
 
 def test_sql_interp_extra_whitespace():
     # Excess whitespace is fine.
-    sql, bind = sql_interp("SELECT ", " *", "   FROM table")
+    sqli = SQLInterp()
+
+    sql, bind = sqli.interp("SELECT ", " *", "   FROM table")
     eq_(sql, "SELECT  *    FROM table")
     eq_(bind, ())
 
 def test_sql_interp_dict():
+    sqli = SQLInterp()
+
     where = { 'first_name': 'John', 'last_name': 'Doe' }
-    sql, bind = sql_interp("SELECT * FROM users WHERE", where)
+    sql, bind = sqli.interp("SELECT * FROM users WHERE", where)
     eq_(sql, "SELECT * FROM users WHERE first_name = ? AND last_name = ?")
     eq_(bind, ('John', 'Doe'))
 
     where = { 'first_name': ['John', 'Jane'], 'last_name': 'Doe' }
-    sql, bind = sql_interp("SELECT * FROM users WHERE", where)
+    sql, bind = sqli.interp("SELECT * FROM users WHERE", where)
     eq_(sql, "SELECT * FROM users WHERE first_name IN (?, ?) AND last_name = ?")
     eq_(bind, ('John', 'Jane', 'Doe'))
 
 def test_sql_interp_dict_none():
+    sqli = SQLInterp()
+
     where = { 'first_name': None, 'last_name': 'Doe' }
-    sql, bind = sql_interp("SELECT * FROM users WHERE", where)
+    sql, bind = sqli.interp("SELECT * FROM users WHERE", where)
     eq_(sql, "SELECT * FROM users WHERE first_name IS NULL AND last_name = ?")
     eq_(bind, ('Doe',))
 
 def test_sql_interp_tuple():
     # Tuples are treated identically to lists.
+    sqli = SQLInterp()
+
     cols = ('one', 'two', 'three')
-    sql, bind = sql_interp("SELECT", cols, "FROM table")
+    sql, bind = sqli.interp("SELECT", cols, "FROM table")
     eq_(sql, "SELECT (?, ?, ?) FROM table")
     eq_(bind, ('one', 'two', 'three'))
 
 def test_sql_interp_string():
+    sqli = SQLInterp()
+
     full_name = 'John Doe'
-    sql, bind = sql_interp("SELECT * FROM table WHERE full_name =", esc(full_name))
+    sql, bind = sqli.interp("SELECT * FROM table WHERE full_name =", sqli.esc(full_name))
     eq_(sql, "SELECT * FROM table WHERE full_name = ?")
     eq_(bind, ('John Doe',))
