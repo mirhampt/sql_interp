535334832220ab836ab012fe820bf79f92610e56
Michael Hampton <mirhampt@gmail.com>
2009-10-27 20:07:38 -0400

    Write docstrings for SQLInterp class

--- sql_interp.py
+++ sql_interp.py
@@ -3,6 +3,9 @@ from esc_types import Esc, ListEsc, DictEsc
 class UnknownTypeError(Exception): pass
 
 class SQLInterp(object):
+    """
+    The main sql_interp object.
+    """
     TYPE_MAP = { 
         list: ListEsc,
         tuple: ListEsc,
@@ -13,6 +16,10 @@ class SQLInterp(object):
         pass
 
     def interp(self, *args):
+        """
+        This method takes a list of SQL snippets and returns a SQL statement and
+        a list of bind variables to be passed to the DB API's execute method.
+        """
         sql = ""
         bind = ()
 
@@ -45,6 +52,27 @@ class SQLInterp(object):
         return (sql, bind)
 
     def esc(self, val):
+        """
+        Returns the given object in the appropriate wrapper class from esc_types.py.
+
+        In most cases, you will not need to call this directly.  However, if you are
+        passing a string to the interp method that should be used as an SQL bind value
+        and not raw SQL, you must pass it to this method to avoid a SQL injection
+        vulnerability.  For example:
+
+        >>> sqli = SQLInterp()
+        >>> first_name = 'John'
+
+        The following is wrong!  This could lead to a SQL injection attack.
+
+        >>> sqli.interp("SELECT * FROM table WHERE first_name =", first_name)
+        ('SELECT * FROM table WHERE first_name = John', ())
+
+        This is the correct way.
+
+        >>> sqli.interp("SELECT * FROM table WHERE first_name =", sqli.esc(first_name))
+        ('SELECT * FROM table WHERE first_name = ?', ('John',))
+        """
         if type(val) in self.TYPE_MAP:
             return self.TYPE_MAP[type(val)](val)
         else:
